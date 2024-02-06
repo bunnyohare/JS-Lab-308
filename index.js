8cafb20f3113b2d3d3feb3f1b572eabf43179153
bunnyohare <739120+bunnyohare@users.noreply.github.com>
2024-02-06 16:01:25 -0500

    First number check addes.

--- index.js
+++ index.js
@@ -43,6 +43,9 @@ const dontDoThis = ((n1 + n2 + n3 + n4) == 50) &&
   ((n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2) && 
   !(n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25) && 
   (n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4);
-
+//•	Check if all numbers are divisible by 5. Cache the result in a variable.
   let fiveMultiple = (n1 % 5) + (n2 % 5) + (n3 % 5) + (n4 % 5) == 0; 
   console.log(fiveMultiple);
+  //•	Check if the first number is larger than the last. Cache the result in a variable.
+ let firstLastComp =(n1 > n4);
+ console.log(firstLastComp);
