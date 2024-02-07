d2290cbf95904ae79b455d596245c2cfc1b41e9e
bunnyohare <739120+bunnyohare@users.noreply.github.com>
2024-02-06 20:06:46 -0500

    added cross country trip.

--- index.js
+++ index.js
@@ -68,3 +68,69 @@ console.log(isUnder25);
 const newIsValid = isSum50 && isTwoOdd && isUnder25 && isUnique;
 
 console.log(newIsValid);
+
+
+/*
+•	The distance of the trip, in total, is 1,500 miles.
+•	Your car’s fuel efficiency is as follows:
+o	At 55 miles per hour, you get 30 miles per gallon.
+o	At 60 miles per hour, you get 28 miles per gallon.
+o	At 75 miles per hour, you get 23 miles per gallon.
+•	You have a fuel budget of $175.
+•	The average cost of fuel is $3 per gallon.
+Set up a program to answer the following questions:
+•	How many gallons of fuel will you need for the entire trip?
+•	Will your budget be enough to cover the fuel expense?
+•	How long will the trip take, in hours?
+Compare the results when traveling at an average of 55, 60, and 75 miles per hour. Which makes the most sense for the trip?
+Log the results of your calculations using string concatenation or template literals.
+*/
+
+const distance = 1500;
+const mpgFiftyFive = 30;
+const mpgSixty = 28;
+const mpgSeventyFive = 23;
+const budget = 175;
+const gasPrice = 3;
+const fiftyF = 55;
+const sixty = 60;
+const seventyF = 75;
+
+
+
+let galPerTripFF = distance / mpgFiftyFive;
+console.log(galPerTripFF +"gallons of gas at 55 mph");
+
+let costFF = galPerTripFF * gasPrice;
+console.log("$" + costFF + " to drive at 55mph.");
+
+let timeFF = distance/fiftyF;
+console.log("It will take " + timeFF +"hours for the trip at " + fiftyF + "miles per hour.")
+
+
+let galPerTripS = distance / mpgSixty;
+console.log(galPerTripS + " gallons of gas needed for optimum trip");
+
+let costS = galPerTripS * gasPrice;
+console.log("$" + costS + " to drive at 60mph. Best value for time within the given cost.");
+
+let timeS = distance/sixty;
+console.log("It will take " + timeS +"hours for the trip at " + sixty + "miles per hour.");
+
+let galPerTripSF = distance/mpgSeventyFive;
+console.log(galPerTripSF);
+
+let costSF = galPerTripSF*gasPrice;
+console.log("$" + costSF + " to drive at 75mph.  Too Expennsive");
+
+let timeSF = distance/seventyF;
+console.log("It would take " + timeSF +" hours for the trip at " + seventyF + " miles per hour if we had the budget for that speed.")
+
+
+let gallonsNeeded = distance/mpgSixty;
+console.log("We need " + gallonsNeeded + "gallons of gas to take the trip at 60mph.")
+
+let travelTime = distance / sixty;
+console.log("It will take " + travelTime + " hours to make the trip.")
+
+
